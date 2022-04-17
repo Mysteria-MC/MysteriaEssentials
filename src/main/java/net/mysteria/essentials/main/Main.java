d648c834865f115638462fe03dff5bae3007443a
simon budzus <simonbudzus@t-online.de>
2022-04-17 19:32:45 +0200

    working version

--- src/main/java/net/mysteria/essentials/main/Main.java
+++ src/main/java/net/mysteria/essentials/main/Main.java
@@ -17,8 +17,8 @@ public class Main extends JavaPlugin {
 		
 		loadConfig();
 		
-		PersistenceXMLConfig persistenceXMLConfig = PersistenceXMLConfig.getInstance(this);
-		persistenceXMLConfig.writePersistenceXML();
+//		PersistenceXMLConfig persistenceXMLConfig = PersistenceXMLConfig.getInstance(this);
+//		persistenceXMLConfig.writePersistenceXML();
 		
 		new GMode(this);
 		new Vanish(this);
